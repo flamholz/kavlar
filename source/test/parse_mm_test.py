6f6b5faa7e5238e91020d4fb2ecade94f6f35113
Avi Flamholz <flamholz@gmail.com>
2016-09-20 19:41:21 -0400

    Even more rigorous test of Bereshit parsing output.

--- source/test/parse_mm_test.py
+++ source/test/parse_mm_test.py
@@ -4,7 +4,7 @@
 import unittest
 
 from parse_mm import MechonMamreParser
-from torah_model import Perek
+from torah_model import Perek, TextFragment
 
 
 class MechonMamreParserTest(unittest.TestCase):
@@ -20,6 +20,19 @@ class MechonMamreParserTest(unittest.TestCase):
 		expected_perakim = 50  # Bereshit has 50
 		self.assertEquals(expected_perakim, n_perakim)
 
+		# For a particular perek, check it has the right
+		# number of psukim - same as number of TextFragments
+		# except in very strange cases where psukim are broken
+		# with a petuha/setumah which does not happen in Bereshit.
+		random_perek = 13     # index 13 is perek 14
+		expected_psukim = 24  # has 24 psukim
+		actual_psukim = 0
+		perek = sefer.stream[random_perek]
+		for elt in perek.iter_stream:
+			if type(elt) == TextFragment:
+				actual_psukim += 1
+		self.assertEquals(expected_psukim, actual_psukim)
+		
 
 if __name__ == '__main__':
 	unittest.main()
